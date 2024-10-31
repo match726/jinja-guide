ff80921c63ca2ef67942189a64a2584309dfa51d
match726 <zildjian0726@gmail.com>
2024-10-31 22:52:26 +0900

    first commit

--- server/api/index.go
+++ server/api/index.go
@@ -6,5 +6,5 @@ import (
 )
 
 func Stdareacode(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Golang!!</h1>")
+	fmt.Fprintf(w, fmt.Sprintf("<p>Hello from Golang!! %s</p>", r))
 }
