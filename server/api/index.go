1cd0cdcc60c78a908f4fa42035260fcd3357e356
match726 <zildjian0726@gmail.com>
2024-11-02 23:04:59 +0900

    first commit

--- server/api/index.go
+++ server/api/index.go
@@ -3,11 +3,8 @@ package api
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/match726/jinja-guide/tree/main/server/models"
 )
 
 func Stdareacode(w http.ResponseWriter, r *http.Request) {
-	models.Run().Exit()
 	fmt.Fprintf(w, fmt.Sprintf("<p>Hello from Golang!! %s</p>", r))
 }
