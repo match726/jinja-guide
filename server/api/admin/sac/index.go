a7428333e8b19a3e3d26453653d347166ba43c68
match726 <zildjian0726@gmail.com>
2024-11-03 18:24:14 +0900

    first commit

--- server/api/admin/sac/index.go
+++ server/api/admin/sac/index.go
@@ -5,6 +5,15 @@ import (
 	"net/http"
 )
 
-func Stdareacode(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, fmt.Sprintf("<p>%s</p>", r))
+func StdAreaCodeHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		FetchStdAreaCodes(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func FetchStdAreaCodes(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("標準地域コード取得")
 }
