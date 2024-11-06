b675cb938dcda88e7cbe74388ffd6bd27c8061d8
match726 <zildjian0726@gmail.com>
2024-11-07 03:07:30 +0900

    first commit

--- server/models/stdareacode.go
+++ server/models/stdareacode.go
@@ -256,69 +256,70 @@ PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>`
 				FILTER ( ?adclass = sacs:District )
 				}`
 
-	// 振興局・支庁に属する、北海道の町村の抽出 (北海道の振興局)
-	query6 := `  UNION
-					{
-					?s a sacs:StandardAreaCode ;
-						dcterms:identifier ?areacode ;
-						dcterms:isPartOf / dcterms:isPartOf / dcterms:identifier ?psac ;
-						dcterms:isPartOf / dcterms:identifier ?spsac ;
-						dcterms:identifier ?m2sac ;
-						sacs:prefectureLabel ?pref ;
-						dcterms:isPartOf ?spo ;
-						sacs:districtOfSubPrefecture ?munic1 ;
-						ic:表記 ?munic2 ;
-						sacs:administrativeClass ?adclass1 ;
-						dcterms:isPartOf / sacs:administrativeClass ?adclass2 .
-					?spo ic:表記 ?subpref .
-					FILTER REGEX ( ?pref, "北海道")
-					FILTER ( lang(?subpref) = "ja" )
-					FILTER ( lang(?munic1) = "ja" )
-					FILTER ( lang(?munic2) = "ja" )
-					FILTER ( ?adclass1 IN( sacs:Town, sacs:Village, sacs:Ward ) )
-					FILTER ( ?adclass2 = sacs:SubPrefecture )
-					MINUS { ?spo dcterms:valid ?spo2 }
-					}`
 	/*
-		// 振興局・支庁に属する、東京の町村の抽出 (東京の離島)
-		query7 := `  UNION
-					{
-					?s a sacs:StandardAreaCode ;
-						dcterms:identifier ?areacode ;
-						dcterms:isPartOf / dcterms:isPartOf / dcterms:identifier ?psac ;
-						dcterms:isPartOf / dcterms:identifier ?spsac ;
-						dcterms:identifier ?m2sac ;
-						sacs:prefectureLabel ?pref ;
-						dcterms:isPartOf ?spo ;
-						ic:表記 ?munic2 ;
-						sacs:administrativeClass ?adclass1 ;
-						dcterms:isPartOf / sacs:administrativeClass ?adclass2 .
-					?spo ic:表記 ?subpref .
-					FILTER REGEX ( ?pref, "東京")
-					FILTER ( lang(?subpref) = "ja" )
-					FILTER ( lang(?munic2) = "ja" )
-					FILTER ( ?adclass1 IN( sacs:Town, sacs:Village, sacs:Ward ) )
-					FILTER ( ?adclass2 = sacs:SubPrefecture )
-					MINUS { ?spo dcterms:valid ?spo2 }
-					}`
+		// 振興局・支庁に属する、北海道の町村の抽出 (北海道の振興局)
+		query6 := `  UNION
+						{
+						?s a sacs:StandardAreaCode ;
+							dcterms:identifier ?areacode ;
+							dcterms:isPartOf / dcterms:isPartOf / dcterms:identifier ?psac ;
+							dcterms:isPartOf / dcterms:identifier ?spsac ;
+							dcterms:identifier ?m2sac ;
+							sacs:prefectureLabel ?pref ;
+							dcterms:isPartOf ?spo ;
+							sacs:districtOfSubPrefecture ?munic1 ;
+							ic:表記 ?munic2 ;
+							sacs:administrativeClass ?adclass1 ;
+							dcterms:isPartOf / sacs:administrativeClass ?adclass2 .
+						?spo ic:表記 ?subpref .
+						FILTER REGEX ( ?pref, "北海道")
+						FILTER ( lang(?subpref) = "ja" )
+						FILTER ( lang(?munic1) = "ja" )
+						FILTER ( lang(?munic2) = "ja" )
+						FILTER ( ?adclass1 IN( sacs:Town, sacs:Village, sacs:Ward ) )
+						FILTER ( ?adclass2 = sacs:SubPrefecture )
+						MINUS { ?spo dcterms:valid ?spo2 }
+						}`
 
-		// 振興局・支庁に属さない、区町村の抽出
-		query8 := `  UNION
-					{
-					?s a sacs:StandardAreaCode ;
-						dcterms:identifier ?areacode ;
-						dcterms:isPartOf / dcterms:isPartOf / dcterms:identifier ?psac ;
-						dcterms:isPartOf / dcterms:identifier ?m1sac ;
-						dcterms:identifier ?m2sac ;
-						sacs:prefectureLabel ?pref ;
-						dcterms:isPartOf / ic:表記 ?munic1 ;
-						ic:表記 ?munic2 ;
-						sacs:administrativeClass ?adclass1 ;
-						dcterms:isPartOf / sacs:administrativeClass ?adclass2 .
-					FILTER ( lang(?munic1) = "ja" )
-					FILTER ( ?adclass1 IN( sacs:Town, sacs:Village, sacs:Ward ) )
-					FILTER ( ?adclass2 != sacs:SubPrefecture )
-					}`
+			// 振興局・支庁に属する、東京の町村の抽出 (東京の離島)
+			query7 := `  UNION
+						{
+						?s a sacs:StandardAreaCode ;
+							dcterms:identifier ?areacode ;
+							dcterms:isPartOf / dcterms:isPartOf / dcterms:identifier ?psac ;
+							dcterms:isPartOf / dcterms:identifier ?spsac ;
+							dcterms:identifier ?m2sac ;
+							sacs:prefectureLabel ?pref ;
+							dcterms:isPartOf ?spo ;
+							ic:表記 ?munic2 ;
+							sacs:administrativeClass ?adclass1 ;
+							dcterms:isPartOf / sacs:administrativeClass ?adclass2 .
+						?spo ic:表記 ?subpref .
+						FILTER REGEX ( ?pref, "東京")
+						FILTER ( lang(?subpref) = "ja" )
+						FILTER ( lang(?munic2) = "ja" )
+						FILTER ( ?adclass1 IN( sacs:Town, sacs:Village, sacs:Ward ) )
+						FILTER ( ?adclass2 = sacs:SubPrefecture )
+						MINUS { ?spo dcterms:valid ?spo2 }
+						}`
+
+			// 振興局・支庁に属さない、区町村の抽出
+			query8 := `  UNION
+						{
+						?s a sacs:StandardAreaCode ;
+							dcterms:identifier ?areacode ;
+							dcterms:isPartOf / dcterms:isPartOf / dcterms:identifier ?psac ;
+							dcterms:isPartOf / dcterms:identifier ?m1sac ;
+							dcterms:identifier ?m2sac ;
+							sacs:prefectureLabel ?pref ;
+							dcterms:isPartOf / ic:表記 ?munic1 ;
+							ic:表記 ?munic2 ;
+							sacs:administrativeClass ?adclass1 ;
+							dcterms:isPartOf / sacs:administrativeClass ?adclass2 .
+						FILTER ( lang(?munic1) = "ja" )
+						FILTER ( ?adclass1 IN( sacs:Town, sacs:Village, sacs:Ward ) )
+						FILTER ( ?adclass2 != sacs:SubPrefecture )
+						}`
 	*/
 	/*
 		// 東京２３区の抽出
@@ -343,7 +344,7 @@ PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>`
 }
 ORDER BY ?areacode`
 
-	query := prefix + query4 + query5 + query6 + query10
+	query := prefix + query4 + query5 + query10
 
 	resp := QuerySparql(os.Getenv("ESTAT_ENDPOINT"), query)
 
