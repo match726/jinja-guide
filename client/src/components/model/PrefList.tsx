6dc1f3fa33bb660b964513ba92f73ac5f56e0cce
match726 <zildjian0726@gmail.com>
2024-11-11 22:52:50 +0900

    first commit

--- client/src/components/model/PrefList.tsx
+++ client/src/components/model/PrefList.tsx
@@ -16,13 +16,14 @@ type SacRelationship = {
   has_child: boolean
 };
 
-//const FRONTEND_URL=import.meta.env.VITE_FRONTEND_URL;
+const FRONTEND_URL=import.meta.env.VITE_FRONTEND_URL;
 const BACKEND_ENDPOINT=import.meta.env.VITE_BACKEND_ENDPOINT;
 
 const Prefs = () => {
 
   const [sacrs, setSacrs] = useState<SacRelationship[]>([]);
 
+  // 標準地域コードの関係性リストを取得
   useEffect(() => {
 
     const options = {
@@ -53,11 +54,11 @@ const Prefs = () => {
     return (
       <Collapsible open={isOpen} onOpenChange={setIsOpen}>
         <CollapsibleTrigger asChild>
-          <Button variant="ghost" className="w-full justify-start p-2">
+          <Button variant="ghost" className="w-full justify-start p-2" asChild>
             {sacr.has_child && (
               isOpen ? <ChevronDown className="mr-2 h-4 w-4" /> : <ChevronRight className="mr-2 h-4 w-4" />
             )}
-            {sacr.name}
+            <a href={FRONTEND_URL + "?kinds=" + sacr.kinds + "&sac=" + sacr.std_area_code}>{sacr.name}</a>
           </Button>
         </CollapsibleTrigger>
         {sacr.has_child && (
@@ -69,14 +70,13 @@ const Prefs = () => {
         )}
       </Collapsible>
     )
-  };  
-
-  console.log(sacrs);
+  };
 
   return (
     <>
       <Header />
       <div className="w-full max-w-md mx-auto p-4 space-y-2 border rounded-lg shadow-sm">
+        <h2 className="text-2xl font-bold mb-4">エリアから検索</h2>
         {sacrs.filter((row: SacRelationship) => row.kinds === "Pref").map((elem: SacRelationship) => (
           <RenderPrefNode key={elem.std_area_code} sacr={elem} />
         ))}
