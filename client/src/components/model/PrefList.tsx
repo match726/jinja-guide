569bf2753406557866c4c99c5fb8281d7e122bbb
match726 <zildjian0726@gmail.com>
2024-11-11 23:11:39 +0900

    first commit

--- client/src/components/model/PrefList.tsx
+++ client/src/components/model/PrefList.tsx
@@ -55,11 +55,13 @@ const Prefs = () => {
     return (
       <Collapsible open={isOpen} onOpenChange={setIsOpen}>
         <CollapsibleTrigger asChild>
-          <Button variant="ghost" className="w-full justify-start p-2" asChild>
-            {sacr.has_child && (
-              isOpen ? <ChevronDown className="mr-2 h-4 w-4" /> : <ChevronRight className="mr-2 h-4 w-4" />
-            )}
-            <Link to={FRONTEND_URL + "?kinds=" + sacr.kinds + "&sac=" + sacr.std_area_code}>{sacr.name}</Link>
+          <Button asChild variant="ghost" className="w-full justify-start p-2">
+            <>
+              {sacr.has_child && (
+                isOpen ? <ChevronDown className="mr-2 h-4 w-4" /> : <ChevronRight className="mr-2 h-4 w-4" />
+              )}
+              <Link to={FRONTEND_URL + "?kinds=" + sacr.kinds + "&code=" + sacr.std_area_code}>{sacr.name}</Link>
+            </>
           </Button>
         </CollapsibleTrigger>
         {sacr.has_child && (
@@ -76,8 +78,10 @@ const Prefs = () => {
   return (
     <>
       <Header />
+      <h1 className="text-[min(4vw,30px)] flex py-4 items-center justify-center">
+        エリアから検索
+      </h1>
       <div className="w-full max-w-md mx-auto p-4 space-y-2 border rounded-lg shadow-sm">
-        <h2 className="text-2xl font-bold mb-4">エリアから検索</h2>
         {sacrs.filter((row: SacRelationship) => row.kinds === "Pref").map((elem: SacRelationship) => (
           <RenderPrefNode key={elem.std_area_code} sacr={elem} />
         ))}
