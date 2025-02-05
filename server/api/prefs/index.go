d5e6466e82506fc4344ea065500f2d05f00db8cf
match726 <zildjian0726@gmail.com>
2025-02-06 00:51:38 +0900

    first commit

--- server/api/prefs/index.go
+++ server/api/prefs/index.go
@@ -1,17 +1,20 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/match726/jinja-guide/tree/main/server/infrastructure/database"
 	logger "github.com/match726/jinja-guide/tree/main/server/infrastructure/log"
+	"github.com/match726/jinja-guide/tree/main/server/infrastructure/persistence"
 	"github.com/match726/jinja-guide/tree/main/server/infrastructure/trace"
 	"github.com/match726/jinja-guide/tree/main/server/usecase"
 )
 
 type PrefsHandler interface {
-	Handler(w http.ResponseWriter, r *http.Request) error
+	Handler(ctx context.Context, w http.ResponseWriter, r *http.Request)
 }
 
 type prefsHandler struct {
@@ -22,7 +25,7 @@ func NewPrefsHandler(saclu usecase.StdAreaCodeListUsecase) PrefsHandler {
 	return &prefsHandler{saclu: saclu}
 }
 
-func (ph prefsHandler) Handler(w http.ResponseWriter, r *http.Request) error {
+func ExportedHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("PrefsHandler")
 	fmt.Println("http.Request: %s", r)
@@ -30,12 +33,12 @@ func (ph prefsHandler) Handler(w http.ResponseWriter, r *http.Request) error {
 	switch r.Method {
 	case http.MethodOptions:
 		w.WriteHeader(http.StatusOK)
-		return nil
+		return
 	case http.MethodGet:
 		break
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		return nil
+		return
 	}
 
 	// Contextを生成
@@ -44,9 +47,31 @@ func (ph prefsHandler) Handler(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 		logger.Error(ctx, "トレーサープロバイダー作成失敗", "errmsg", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	defer shutdown(ctx)
 	ctx = trace.GetContextWithTraceID(r.Context(), "PrefsHandler")
+
+	var pg *database.Postgres
+
+	pg, err = database.NewPool(ctx)
+
+	if err != nil {
+		logger.Error(ctx, "コネクションプール作成失敗", "errmsg", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer pg.ClosePool(ctx)
+
+	saclp := persistence.NewStdAreaCodeListPersistence(pg)
+	saclu := usecase.NewStdAreaCodeListUsecase(saclp)
+	ph := NewPrefsHandler(saclu)
+
+	ph.Handler(ctx, w, r)
+
+}
+
+func (ph prefsHandler) Handler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 
 	// 登録されている神社を元に都道府県の標準地域コード（紐付き）を取得する
 	sacrrs, err := ph.saclu.GetAllStdAreaCodeRelationshipList(ctx)
@@ -68,7 +93,5 @@ func (ph prefsHandler) Handler(w http.ResponseWriter, r *http.Request) error {
 	if _, err := w.Write(b); err != nil {
 		logger.Error(ctx, "Body書込失敗", "errmsg", err)
 	}
-
-	return nil
 
 }
