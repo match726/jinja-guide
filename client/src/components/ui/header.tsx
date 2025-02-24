12e30b151896a23b6a321e9ae019f5084b325361
match726 <zildjian0726@gmail.com>
2025-02-24 19:39:20 +0900

    first commit

--- client/src/components/ui/header.tsx
+++ client/src/components/ui/header.tsx
@@ -25,6 +25,9 @@ const Header: React.FC = () => {
               />
             </svg>
             <a href="/" className="text-2xl font-semibold">神社ガイド</a>
+            if ({import.meta.env.VITE_ENV_KEY} === "DEVELOP" ) {
+              <p>開発環境</p>
+            }
           </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
