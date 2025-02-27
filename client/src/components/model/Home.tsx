eaa3e9f211fa759cafdfa55819d06c56b946021f
match726 <zildjian0726@gmail.com>
2025-02-28 01:12:16 +0900

    first commit

--- client/src/components/model/Home.tsx
+++ client/src/components/model/Home.tsx
@@ -8,25 +8,21 @@ import '@/styles/global.css';
 
 const backendEndpoint = import.meta.env.VITE_BACKEND_ENDPOINT;
 
-interface RandomShrines {
-  name: string
-  furigana: string
-  address: string
-  plusCode: string
-  placeId: string
-  objectOfWorship: string[]
+type RandomShrines = {
+  name: string,
+  furigana: string,
+  address: string,
+  plusCode: string,
+  placeId: string,
+  objectOfWorship: string[],
   description: string
-}
-
-interface HomeContents {
-  shrines: RandomShrines[]
 }
 
 const frontendUrl = import.meta.env.VITE_FRONTEND_URL;
 
 const Home: React.FC = () => {
 
-  const [contents, setContents] = useState<HomeContents>({shrines: []});
+  const [shrines, setShrines] = useState<RandomShrines[]>();
 
   useEffect(() => {
 
@@ -42,9 +38,7 @@ const Home: React.FC = () => {
       try {
         const resp = await axios(reqOptions);
         console.log("HTTPレスポンス: ", resp.data)
-        setContents((prevContents) => ({
-          ...prevContents, shrines: [resp.data.RandomShrine]
-        }));
+        setShrines(resp.data.RandomShrine);
       } catch (error) {
         console.error("GETリクエスト失敗", error);
       }
@@ -54,7 +48,7 @@ const Home: React.FC = () => {
 
   }, []);
 
-  console.log(contents.shrines)
+  console.log(shrines)
 
   return (
     <div>
@@ -64,7 +58,7 @@ const Home: React.FC = () => {
           神社（ランダム表示）
         </h1>
         <section className="container flex grid grid-cols-2 gap-10 xl:grid-cols-3">
-          {contents.shrines.map((data) => (
+          {shrines && shrines.map((data) => (
             <AdminCard
               cardTitle={data.name}
               cardTitleRuby={data.furigana}
