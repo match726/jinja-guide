55aab9ae5168938f1bd2e15e58476c05fd1a145a
match726 <zildjian0726@gmail.com>
2025-02-27 20:57:28 +0900

    first commit

--- client/src/components/model/Home.tsx
+++ client/src/components/model/Home.tsx
@@ -1,11 +1,74 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import { Header } from '@/components/ui/header';
+import { AdminCard } from '@/components/ui/card/admin-card';
 
 import '@/styles/global.css';
 
+const backendEndpoint = import.meta.env.VITE_BACKEND_ENDPOINT;
+
+interface randomShrines {
+  name: string
+  furigana: string
+  address: string
+  plusCode: string
+  placeId: string
+  objectOfWorship: string[]
+  description: string
+}
+
+type homeContents = {
+  shrines: randomShrines[],
+}
+
+const frontendUrl = import.meta.env.VITE_FRONTEND_URL;
+
 const Home: React.FC = () => {
+
+  const [contents, setContents] = useState<homeContents>({shrines: []});
+
+  useEffect(() => {
+
+    const reqOptions = {
+      method: "GET",
+      url: backendEndpoint + "/api/home",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    };
+
+    const fetchHomeContents = async () => {
+      try {
+        const resp = await axios(reqOptions);
+        console.log("HTTPレスポンス: ", resp.data)
+        setContents(resp.data);
+      } catch (error) {
+        console.error("GETリクエスト失敗", error);
+      }
+    };
+
+    fetchHomeContents();
+
+  }, []);
+
   return (
     <div>
       <Header />
+      <div className="bg-gradient-to-b from-red-50 to-white">
+        <h1 className="text-[min(4vw,30px)] flex py-4 items-center justify-center">
+          神社（ランダム表示）
+        </h1>
+        <section className="container flex grid grid-cols-2 gap-10 xl:grid-cols-3">
+          {contents.shrines.map((data) => (
+            <AdminCard
+              cardTitle={data.name}
+              cardDescription={data.description}
+              cardLink={frontendUrl + "/shrine?code=" + data.plusCode}
+            />
+          ))}
+        </section>
+      </div>
     </div>
   );
 };
