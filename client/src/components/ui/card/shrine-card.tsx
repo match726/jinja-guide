bcd34445df9a2dd71d56c3f8f17111f0aaa08260
match726 <zildjian0726@gmail.com>
2025-03-03 21:32:45 +0900

    first commit

--- client/src/components/ui/card/shrine-card.tsx
+++ client/src/components/ui/card/shrine-card.tsx
@@ -15,7 +15,7 @@ type Props = {
 const ShrineCard: React.FC<Props> = (props) => {
   return (
     <Link href={props.cardLink} passHref>
-      <Card className="w-full max-w-md mx-auto bg-white dark:bg-gray-800 overflow-hidden">
+      <Card className="w-full max-w-md mx-auto bg-white dark:bg-gray-800 overflow-hidden hover:bg-red-200">
         <CardContent className="p-6">
           <div className="flex items-center mb-4">
             <div className="relative w-20 h-20 mr-4 bg-gray-100 dark:bg-gray-700 rounded-lg overflow-hidden flex-shrink-0">
