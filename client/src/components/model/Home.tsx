9e615b956094fcbbe9ffde64f3637f94f2acfdc9
match726 <zildjian0726@gmail.com>
2025-03-09 21:14:41 +0900

    first commit

--- client/src/components/model/Home.tsx
+++ client/src/components/model/Home.tsx
@@ -90,7 +90,7 @@ const Home: React.FC = () => {
               </TabsTrigger>
             </TabsList>
             <TabsContent value="0" className="py-6 flex justify-center">
-              <div className="absolute w-full max-w-md bottom-1/4">
+              <div className="absolute w-full max-w-md bottom-1/3">
                 <div className="relative w-full max-w-md">
                   <Input
                     type="search"
