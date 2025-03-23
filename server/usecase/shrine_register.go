ed8b0091233f445e17e4200a583babbb5e8b3e68
match726 <zildjian0726@gmail.com>
2025-03-23 21:13:37 +0900

    first commit

--- server/usecase/shrine_register.go
+++ server/usecase/shrine_register.go
@@ -75,6 +75,9 @@ func (sru shrineRegisterUsecase) GetAllRegisterShrines(ctx context.Context) (psh
 			shrineRankFields = append(shrineRankFields, model.Field{Id: idx + 1, Seq: shrineRank.Seq, Content1: shrineRank.Content1, Content2: shrineRank.Content2, Content3: shrineRank.Content3})
 		}
 
+		fmt.Println(len(shrr.FoundedYear.Content1))
+		fmt.Println(len(shrr.FoundedYear.Content2))
+		fmt.Println(len(shrr.FoundedYear.Content3))
 		shrq := model.ShrineRegisterReq{
 			Name:             []model.Field{{Id: 1, Seq: "", Content1: shrr.Name, Content2: "", Content3: ""}},
 			Address:          []model.Field{{Id: 1, Seq: "", Content1: shrr.Address, Content2: "", Content3: ""}},
