ad084481ba04f299dc03176e91869f51c6428361
match726 <zildjian0726@gmail.com>
2025-04-02 23:24:07 +0900

    first commit

--- client/src/features/register/components/formField.tsx
+++ client/src/features/register/components/formField.tsx
@@ -1,7 +1,8 @@
 import { SlPlus, SlMinus } from 'react-icons/sl';
 
 import { Button } from '@/components/ui/button';
-import { FieldProps, FormProps } from '@/features/register/types/types';
+import { FieldProps } from '@/types/types';
+import { FormProps } from '@/features/register/types/types';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
